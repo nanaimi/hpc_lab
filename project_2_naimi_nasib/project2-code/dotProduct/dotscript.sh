2944eaee6e42533315c23cb27ff9e8ae102373df
Nasib Naimi <28812908+nanaimi@users.noreply.github.com>
2022-03-17 15:26:36 +0100

    script

--- project_2_naimi_nasib/project2-code/dotProduct/dotscript.sh
+++ project_2_naimi_nasib/project2-code/dotProduct/dotscript.sh
@@ -1,9 +1,9 @@
 for i in {0..4}
 do
   N=$[100000*10**i]
-  for t in {1..4}
+  for t in {1..24}
   do
-    export OMP_NUM_THREADS=t
-    out=$(bsub -n $t -W 00:10 -R "span[ptile=$t]" ./dotProduct $N)
+    export OMP_NUM_THREADS=$t
+    bsub -n $t -W 00:10 -R "span[ptile=$t]" ./dotProduct $N
   done
 done
