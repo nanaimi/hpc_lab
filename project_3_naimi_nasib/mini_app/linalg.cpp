cae576ecece481aea3a73ddae09933c76c4dcd43
Nasib Naimi <28812908+nanaimi@users.noreply.github.com>
2022-04-11 01:11:18 +0200

    fixed linalg

--- project_3_naimi_nasib/mini_app/linalg.cpp
+++ project_3_naimi_nasib/mini_app/linalg.cpp
@@ -66,11 +66,8 @@ double hpc_norm2(Field const& x, const int N)
 {
     double result = 0;
     // TODO
-    for (int i = 0; i < N; i++)
-        result += x[i] * x[i];
+    result = hpc_dot(x, x, N);
     
-    result = sqrt(result);
-
     return sqrt(result);
 }
 
@@ -80,8 +77,9 @@ double hpc_norm2(Field const& x, const int N)
 void hpc_fill(Field& x, const double value, const int N)
 {
     //TODO
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++){
         x[i] = value;
+    }
     
 }
 
@@ -95,8 +93,9 @@ void hpc_fill(Field& x, const double value, const int N)
 void hpc_axpy(Field& y, const double alpha, Field const& x, const int N)
 {
     //TODO
-    for (int i = 0; i < N; i++)
-        y[i] = alpha * x[i] + y[i];
+    for (int i = 0; i < N; i++){
+        y[i] += alpha * x[i];
+    }
 
 }
 
@@ -107,8 +106,9 @@ void hpc_add_scaled_diff(Field& y, Field const& x, const double alpha,
     Field const& l, Field const& r, const int N)
 {
     //TODO
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++) {
         y[i] = x[i] + alpha * (l[i] - r[i]);
+    }
 
 }
 
@@ -119,8 +119,9 @@ void hpc_scaled_diff(Field& y, const double alpha,
     Field const& l, Field const& r, const int N)
 {
     //TODO
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++) {
         y[i] = alpha * (l[i] - r[i]);
+    }
 }
 
 // computes y := alpha*x
@@ -129,8 +130,9 @@ void hpc_scaled_diff(Field& y, const double alpha,
 void hpc_scale(Field& y, const double alpha, Field& x, const int N)
 {
     //TODO
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++) {
         y[i] = alpha * x[i];
+    }
 }
 
 // computes linear combination of two vectors y := alpha*x + beta*z
@@ -140,8 +142,9 @@ void hpc_lcomb(Field& y, const double alpha, Field& x, const double beta,
     Field const& z, const int N)
 {
     //TODO
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++) {
         y[i] = alpha * x[i] + beta * z[i];
+    }
 }
 
 // copy one vector into another y := x
@@ -149,8 +152,9 @@ void hpc_lcomb(Field& y, const double alpha, Field& x, const double beta,
 void hpc_copy(Field& y, Field const& x, const int N)
 {
     //TODO
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++) {
         y[i] = x[i];
+    }
 }
 
 // conjugate gradient solver
