b87a86147cd1ab98711fa0c4593c07dd0d3ca57a
Nasib Naimi <28812908+nanaimi@users.noreply.github.com>
2022-04-11 16:03:17 +0200

    just 1024

--- project_3_naimi_nasib/mini_app/pdescript.sh
+++ project_3_naimi_nasib/mini_app/pdescript.sh
@@ -1,9 +1,9 @@
-for i in {0..4}
-do
-  SIZE=$[64*2**i]
+# for i in {0..4}
+# do
+  SIZE=1024 # $[64*2**i]
   for t in {1..24}
   do
     export OMP_NUM_THREADS=$t
-    bsub -n $t -W 00:10 -R "span[ptile=$t]" -R "select[model=XeonGold_6150]" ./main $SIZE 100 0.005
+    bsub -n $t -W 00:10 -R "span[ptile=$t]" -R "select[model=XeonGold_6150]" ./main $SIZE 100 0.05
   done
-done
+# done
