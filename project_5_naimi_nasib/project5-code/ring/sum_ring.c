0f8809dcc5317d0e5ef4a20f82ffa9e5d2888267
Nasib Naimi <28812908+nanaimi@users.noreply.github.com>
2022-04-28 18:49:08 +0200

    changes sum_ring

--- project_5_naimi_nasib/project5-code/ring/sum_ring.c
+++ project_5_naimi_nasib/project5-code/ring/sum_ring.c
@@ -50,13 +50,15 @@ int main (int argc, char *argv[])
     snd_buf = my_rank;
     for(i = 0; i < size; i++){
 
-        MPI_Isend(&snd_buf,1,MPI_INT,right,0,MPI_COMM_WORLD, &request);
-
-        // MPI_Send(&snd_buf,1,MPI_INT,right,0,MPI_COMM_WORLD);
-
-        MPI_Recv(&rcv_buf,1,MPI_INT,left,0,MPI_COMM_WORLD,&status);
-
-        MPI_Wait(&request, &status);
+        // MPI_Isend(&snd_buf,1,MPI_INT,right,0,MPI_COMM_WORLD, &request);
+	if ((my_rank % 2) == 0) {
+            MPI_Send(&snd_buf,1,MPI_INT,right,0,MPI_COMM_WORLD);
+            MPI_Recv(&rcv_buf,1,MPI_INT,left,0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	} else {
+	    MPI_Recv(&rcv_buf,1,MPI_INT,left,0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	    MPI_Send(&snd_buf,1,MPI_INT,right,0,MPI_COMM_WORLD);
+	}
+        // MPI_Wait(&request, &status);
 
         sum += rcv_buf;
         snd_buf = rcv_buf;
